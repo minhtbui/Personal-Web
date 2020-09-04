7ba80e20510a0b72969ca6ab00563042c6c3288d
minhtbui <troy.bmt@gmail.com>
2020-09-04 23:08:05 +0700

    minor change

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,121 +5,122 @@
 */
 
 (function ($) {
-   var $window = $(window),
-      $body = $('body'),
-      $header = $('#header'),
-      $footer = $('#footer'),
-      $main = $('#main'),
-      settings = {
-         // Parallax background effect?
-         parallax: true,
+	var $window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$footer = $('#footer'),
+		$main = $('#main'),
+		settings = {
+			// Parallax background effect?
+			parallax: true,
 
-         // Parallax factor (lower = more intense, higher = less intense).
-         parallaxFactor: 20,
-      };
+			// Parallax factor (lower = more intense, higher = less intense).
+			parallaxFactor: 20,
+		};
 
-   // Breakpoints.
-   breakpoints({
-      xlarge: ['1281px', '1800px'],
-      large: ['981px', '1280px'],
-      medium: ['737px', '980px'],
-      small: ['481px', '736px'],
-      xsmall: [null, '480px'],
-   });
+	// Breakpoints.
+	breakpoints({
+		xlarge: ['1281px', '1800px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: [null, '480px'],
+	});
 
-   // Play initial animations on page load.
-   $window.on('load', function () {
-      window.setTimeout(function () {
-         $body.removeClass('is-preload');
-      }, 100);
-   });
+	// Play initial animations on page load.
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
-   // Touch?
-   if (browser.mobile) {
-      // Turn on touch mode.
-      $body.addClass('is-touch');
+	// Touch?
+	if (browser.mobile) {
+		// Turn on touch mode.
+		$body.addClass('is-touch');
 
-      // Height fix (mostly for iOS).
-      window.setTimeout(function () {
-         $window.scrollTop($window.scrollTop() + 1);
-      }, 0);
-   }
+		// Height fix (mostly for iOS).
+		window.setTimeout(function () {
+			$window.scrollTop($window.scrollTop() + 1);
+		}, 0);
+	}
 
-   // Footer.
-   breakpoints.on('<=medium', function () {
-      $footer.insertAfter($main);
-   });
+	// Footer.
+	breakpoints.on('<=medium', function () {
+		$footer.insertAfter($main);
+	});
 
-   breakpoints.on('>medium', function () {
-      $footer.appendTo($header);
-   });
+	breakpoints.on('>medium', function () {
+		$footer.appendTo($header);
+	});
 
-   // Header.
+	// Header.
 
-   // Parallax background.
+	// Parallax background.
 
-   // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-   if (browser.name == 'ie' || browser.mobile) settings.parallax = false;
+	// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+	if (browser.name == 'ie' || browser.mobile) settings.parallax = false;
 
-   if (settings.parallax) {
-      breakpoints.on('<=medium', function () {
-         $window.off('scroll.strata_parallax');
-         $header.css('background-position', '');
-      });
+	if (settings.parallax) {
+		breakpoints.on('<=medium', function () {
+			$window.off('scroll.strata_parallax');
+			$header.css('background-position', '');
+		});
 
-      breakpoints.on('>medium', function () {
-         $header.css('background-position', 'left 0px');
+		breakpoints.on('>medium', function () {
+			$header.css('background-position', 'left 0px');
 
-         $window.on('scroll.strata_parallax', function () {
-            $header.css(
-               'background-position',
-               'left ' +
-                  -1 *
-                     (parseInt($window.scrollTop()) / settings.parallaxFactor) +
-                  'px',
-            );
-         });
-      });
+			$window.on('scroll.strata_parallax', function () {
+				$header.css(
+					'background-position',
+					'left ' +
+						-1 *
+							(parseInt($window.scrollTop()) / settings.parallaxFactor) +
+						'px',
+				);
+			});
+		});
 
-      $window.on('load', function () {
-         $window.triggerHandler('scroll');
-      });
-   }
+		$window.on('load', function () {
+			$window.triggerHandler('scroll');
+		});
+	}
 
-   // Main Sections: Two.
+	// Main Sections: Two.
 
-   // Lightbox gallery.
-   $window.on('load', function () {
-      $('#two').poptrox({
-         caption: function ($a) {
-            return $a.next('h3').text();
-         },
-         overlayColor: '#2c2c2c',
-         overlayOpacity: 0.85,
-         popupCloserText: '',
-         popupLoaderText: '',
-         selector: '.work-item a.image',
-         usePopupCaption: true,
-         usePopupDefaultStyling: false,
-         usePopupEasyClose: false,
-         usePopupNav: true,
-         windowMargin: breakpoints.active('<=small') ? 0 : 50,
-      });
-   });
+	// Lightbox gallery.
+	$window.on('load', function () {
+		$('#two').poptrox({
+			caption: function ($a) {
+				return $a.next('h3').text();
+			},
+			overlayColor: '#2c2c2c',
+			overlayOpacity: 0.85,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.work-item a.image',
+			usePopupCaption: true,
+			usePopupDefaultStyling: false,
+			usePopupEasyClose: false,
+			usePopupNav: true,
+			windowMargin: breakpoints.active('<=small') ? 0 : 50,
+		});
+	});
 
-   // Show more btn
-   $(document).ready(function () {
-      $('.moreButton').click(function () {
-         var elem = $('.moreButton').text();
-         if (elem == 'Read More') {
-            //Stuff to do when btn is in the read more state
-            $('.moreButton').text('Read Less');
-            $('.hideContent').slideDown();
-         } else {
-            //Stuff to do when btn is in the read less state
-            $('.moreButton').text('Read More');
-            $('.hideContent').slideUp();
-         }
-      });
-   });
+	// Show more btn
+	$(document).ready(function () {
+		$('.moreButton').click(function () {
+			var elem = $(this).text();
+			console.log(elem);
+			if (elem === 'Read More') {
+				//Stuff to do when btn is in the read more state
+				$('.moreButton').text('Read Less');
+				$('.hideContent').slideDown();
+			} else {
+				//Stuff to do when btn is in the read less state
+				$('.moreButton').text('Read More');
+				$('.hideContent').slideUp();
+			}
+		});
+	});
 })(jQuery);
